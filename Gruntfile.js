ccc9b9410384ee652a95f43adfc761a88fe1bc60
Joris de Wit <joris.w.dewit@gmail.com>
2013-12-07 14:33:00 -0800

    add bump

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,15 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        bump: {
+          options: {
+            files: ['package.json', 'bower.json'],
+            updateConfigs: [],
+            commit: true,
+            commitMessage: 'Release v%VERSION%',
+            commitFiles: ['package.json', 'bower.json']
+          }
+        },
         jshint: {
           options: {
             curly: true,
@@ -84,6 +93,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-angular-templates');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-bump');
 
     grunt.registerTask('default', ['jshint', 'uglify', 'less', 'ngtemplates']);
 };
